a19fca5e7d43b8766f74cbf0239e6318b78897ea
Ryan Moore <ryan@geekportfolio.com>
2014-06-10 10:00:18 -0700

    combine best-known factor bases between runs

--- factorize.sh
+++ factorize.sh
@@ -5,9 +5,11 @@ CURVES=$2
 COUNT=$3
 
 echo 2 >> factorbase.$WINDOW
+cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
+mv tmp_factorbase.$WINDOW factorbase.$WINDOW
 
 for i in $(seq 1 $COUNT) ; do
-    echo "$(date) Starting loop $i"
+    echo "$(date) Starting loop $i for $WINDOW @ $CURVES"
     ./driver.pl --factorbase=factorbase.$WINDOW --curves=$CURVES --shuffle >> $WINDOW.out 2>> $WINDOW.err
     cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
     mv tmp_factorbase.$WINDOW factorbase.$WINDOW
