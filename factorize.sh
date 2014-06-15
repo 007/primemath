c388bf5fdb074b78f77077fe2bec222e96fb419f
Ryan Moore <ryan@geekportfolio.com>
2014-06-14 17:20:32 -0700

    update driver script to use prefilter

--- factorize.sh
+++ factorize.sh
@@ -10,7 +10,7 @@ mv tmp_factorbase.$WINDOW factorbase.$WINDOW
 
 for i in $(seq 1 $COUNT) ; do
     echo "$(date) Starting loop $i for $WINDOW @ $CURVES"
-    ./driver.pl --factorbase=factorbase.$WINDOW --curves=$CURVES --shuffle --constant >> $WINDOW.out 2>> $WINDOW.err
+    ./driver.pl --factorbase=factorbase.$WINDOW --curves=$CURVES --shuffle --prefilter --constant >> $WINDOW.out 2>> $WINDOW.err
     cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
     mv tmp_factorbase.$WINDOW factorbase.$WINDOW
     echo "$(date) Finished loop $i - waiting for command input"
