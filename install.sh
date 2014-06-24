fc830c1e2ce0e9603d1456b3c6d6836a08e4b8a0
Ryan Moore <ryan@geekportfolio.com>
2014-06-24 07:14:06 +0000

    installer script from source, with path workarounds for system libgmp conflict

--- install.sh
+++ install.sh
@@ -3,8 +3,30 @@ export LC_ALL=C
 apt-get update
 apt-get dist-upgrade
 
-#apt-get install liblist-moreutils-perl
-apt-get install libmath-bigint-gmp-perl
+if [ "$(md5sum gmp-6.0.0a.tar.bz2)" == "b7ff2d88cae7f8085bd5006096eed470  gmp-6.0.0a.tar.bz2" ] ; then
+    echo "Using cached gmp"
+else
+    curl https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2 > gmp-6.0.0a.tar.bz2
+fi
+
+if [ "$(md5sum ecm-6.4.4.tar.gz)" == "927712d698ae9e5de71574fb6ee2316c  ecm-6.4.4.tar.gz" ] ; then
+    echo "Using cached ecm"
+else
+    curl https://gforge.inria.fr/frs/download.php/file/32159/ecm-6.4.4.tar.gz > ecm-6.4.4.tar.gz
+fi
+
+rm -rf gmp-6.0.0/
+tar xjf gmp-6.0.0a.tar.bz2
+pushd gmp-6.0.0/
+./configure && make && make check && make install
+popd
+
+rm -rf ecm-6.4.4/
+tar xzf ecm-6.4.4.tar.gz
+pushd ecm-6.4.4/
+./configure --enable-asm-redc --enable-openmp --with-gmp-lib=/usr/local/lib && make && make check && make install
+popd
 
 curl -L http://cpanmin.us | perl - --self-upgrade
-cpanm Data::Dumper Getopt::Long List::Util List::MoreUtils Math::BigInt::GMP Math::Prime::Util Math::Prime::Util::GMP
+LD_LIBRARY_PATH=/usr/local/lib cpanm Data::Dumper Getopt::Long List::Util List::MoreUtils Math::BigInt::GMP Math::Prime::Util Math::Prime::Util::GMP
+
