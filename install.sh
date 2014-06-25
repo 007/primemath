54ca45212a7826a4d195e974ec3f3fcdd20d8101
Ryan Moore <ryan@geekportfolio.com>
2014-06-25 20:23:25 +0000

    updates to installer script, add ubuntu basics and root check

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+# Install primemath factoring driver from scratch
+# instructions:
+#   apt-get update && apt-get install -y git
+#   git clone git@github.com:007/primemath.git
+#   cd primemath
+#   sudo ./install.sh
+
 export LC_ALL=C
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Must run as root" 1>&2
+   exit 100
+fi
+
 apt-get update
 apt-get dist-upgrade
+apt-get install -y build-essential m4
 
 if [ "$(md5sum gmp-6.0.0a.tar.bz2)" == "b7ff2d88cae7f8085bd5006096eed470  gmp-6.0.0a.tar.bz2" ] ; then
     echo "Using cached gmp"
@@ -31,11 +45,11 @@ curl -L http://cpanmin.us | perl - --self-upgrade
 LD_LIBRARY_PATH=/usr/local/lib cpanm Data::Dumper Getopt::Long List::Util List::MoreUtils Math::BigInt::GMP Math::Prime::Util Math::Prime::Util::GMP
 
 mkdir -p /var/primemath/log
-
-# make these hard links so that local updates will propagate
 cp driver.pl /var/primemath/
 cp worktodo.txt factorbase.txt sigmalog.txt /var/primemath/
 cp onboot.sh /var/primemath/
-(crontab -l | grep -v 'onboot /var/primemath/onboot.sh';echo "onboot /var/primemath/onboot.sh") | crontab -
+
+# update crontab, making sure we have a crontab entry to begin with and filtering out $self
+crontab -l >/dev/null 2>&1 && (crontab -l | grep -v 'reboot /var/primemath/onboot.sh';echo "@reboot /var/primemath/onboot.sh") || echo "@reboot /var/primemath/onboot.sh" | crontab -
 (crontab -l | grep -v '/var/primemath/util.sh'; echo "0 * * * * /var/primemath/util.sh") | crontab -
 
