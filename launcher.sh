2c91eb84a24d5b2701b72155030f41c57a03da6f
Ryan Moore <ryan@geekportfolio.com>
2015-02-17 18:15:08 -0800

    simple worker launch script

---
+++ launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in 1 2 3 4; do
+    screen -dmS prime_00$i -t "primemath worker $i" ./factorize.sh 00$i 8 100
+    sleep 135 # time to complete 1 iteration / (number of workers plus 1)
+    # if it takes 300 seconds for one iteration and you have 4 workers, 300 / (4 + 1) = 60s sleep
+    # need the workers + 1 because first worker starts at time = 0
+done
