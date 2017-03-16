d70702dfe80294727b8c993ff70d1975c7101e84
Ryan Moore <ryan@geekportfolio.com>
2017-03-16 13:17:57 -0700

    better launch scripts

--- factorize.sh
+++ factorize.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+FACTORPATH="$(readlink -f $(dirname $0))"
+
+cd ${FACTORPATH}
 WINDOW=$1
 CURVES=$2
 COUNT=$3
+PARALLEL=$4
 
 echo 2 >> factorbase.$WINDOW
 cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
 mv tmp_factorbase.$WINDOW factorbase.$WINDOW
 
-for i in $(seq 1 $COUNT) ; do
-    echo "$(date) Starting loop $i for $WINDOW @ $CURVES"
-    ./driver.pl --factorbase=factorbase.$WINDOW --curves=$CURVES --shuffle --parallel=6 --color >> $WINDOW.out 2>> $WINDOW.err
-    cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
-    mv tmp_factorbase.$WINDOW factorbase.$WINDOW
-done
+echo "$(date) Starting loop $i for $WINDOW @ $CURVES"
+./driver.pl --factorbase=factorbase.$WINDOW --curves=$CURVES --repeat=$COUNT --shuffle --parallel=$PARALLEL --color >> $WINDOW.out 2>> $WINDOW.err
+cat factorbase.* | sort | uniq | sort -n > tmp_factorbase.$WINDOW
+mv tmp_factorbase.$WINDOW factorbase.$WINDOW
--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd /var/primemath
-
-for i in 1 2 3 4 5 6; do
-    screen -dmS prime_00$i -t "primemath worker $i" ./factorize.sh 00$i 5-8 1
+FACTORPATH="$(readlink -f $(dirname $0))"
+MAXCOUNT=4
+for i in $(seq ${MAXCOUNT}); do
+    #./factorize.sh WiNDOW_PREFIX WHICH_CURVES REPEAT_COUNT PARALLELISM
+    screen -dmS prime_00$i -t "primemath worker $i" ${FACTORPATH}/factorize.sh 00$i 6 8 ${MAXCOUNT}
     sleep 135 # time to complete 1 iteration / (number of workers plus 1)
     # if it takes 300 seconds for one iteration and you have 4 workers, 300 / (4 + 1) = 60s sleep
     # need the workers + 1 because first worker starts at time = 0
