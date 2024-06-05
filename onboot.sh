0078eb0e283f1a7f5274109b1edd9348c5e6d3b1
Ryan Moore <rmoore@aurora.tech>
2024-06-05 04:29:48 +0000

    pin affinity per-cpu

--- onboot.sh
+++ onboot.sh
@@ -10,10 +10,13 @@ function ncpu {
 PATH=/usr/sbin:/bin:/usr/bin:/usr/local/bin
 
 if [ -z "$1" ] ; then
-    for i in `seq -f%03.0f $(ncpu)`; do
+    CPUS=$(ncpu)
+    for i in $(seq 0 $((CPUS - 1))); do
+        IPAD=$(printf "%03d" $i)
         # run self with parameter
-        echo "screen -dmS prime_$i -t \"prime math worker $i\" $0 $i"
-        screen -dmS prime_$i -t "prime math worker $i" $0 $i
+        echo "screen -dmS prime_${IPAD} -t \"prime math worker ${IPAD}\" $0 ${IPAD}"
+        # taskset to pin affinity, not strictly necessary but kinda fun
+        taskset --cpu-list ${i} screen -dmS prime_${IPAD} -t "prime math worker ${IPAD}" $0 ${IPAD}
         # wait a few seconds between starting runs
         sleep 600
     done
@@ -27,7 +30,7 @@ else
     touch /var/primemath/factorbase.$WINDOW
     while [ true ] ; do
         echo "Starting process for window.$WINDOW" >> /var/primemath/log/factorlog.log
-        /var/primemath/driver.pl --factorbase=/var/primemath/factorbase.$WINDOW --curves=$CURVES --shuffle --prefilter --constant >> /var/primemath/log/factorlog.log 2>>/var/primemath/log/factorlog.err
+        /var/primemath/driver.pl --factorbase=/var/primemath/factorbase.$WINDOW --curves=$CURVES --shuffle --constant >> /var/primemath/log/factorlog.log 2>>/var/primemath/log/factorlog.err
         cat /var/primemath/factorbase.* | sort | uniq | sort -n > "$TMPFACTORBASE" && \
         mv "$TMPFACTORBASE" /var/primemath/factorbase.$WINDOW
         sleep 3
